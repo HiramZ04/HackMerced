cb4e795ec058d0d04764143c6f5b49267dffaad2
HiramZ04 <nownisvibing@gmail.com>
2026-03-07 15:34:45 -0800

    Advances in hard-coded alerts

--- ML/alerts.py
+++ ML/alerts.py
@@ -1,15 +1,47 @@
+import time
+# TO DO 
+# [] Cuando tengamos la funcion speak( ) vamos a utilizarla para hablar la alerta 
+# [] Agregar funcionalidad para poder no pisar alertas y hacerlo por orden de prioridad
 
-# Cuando tengamos la funcion speak( ) vamos a utilizarla para hablar la alerta 
+
+last_time_alert = {}
+
 def alerts_constant(tipo, distancia): # We pass the parameter to the function only the buffer last log type of object and the distance
-    if tipo == "person" and distancia < 3:
+
+    now = time.now()
+
+    if tipo in last_time_alert:
+        if last_time_alert - now < 5:
+            return
+
+# If the time from the last alert was less then 5 seconds then we just exit the function and put alerted value at False
+    alerted = True
+
+# We define the alerts, we use elif so the conditional stops at the first true value
+
+    if tipo == "person" and distancia < 1.5:
         print("Stop, there is a person 3 meters in front of you, lets wait for the path to be clear")
-    if tipo == "car" and distancia < 5:
-        print("Stop!!!1 there is a car way to close!!!")
-    if tipo == "stop sign" and distancia < 4:
+    elif tipo == "car" and distancia < 5:
+        print("Stop!!! there is a car way to close!!!")
+    elif tipo == "chair" and distancia < 1.5:
+        print("Chair in front of you, watch out")
+    elif tipo == "dining table" and distancia < 1.5:
+        print("Table ahead, slow down")
+    elif tipo == "couch" and distancia < 1.0:
+        print("Couch ahead")
+    elif tipo == "stop sign" and distancia < 4:
         print("Let's make a stop here, there is a stop sign!, lets make sure there is no danger")
-    if tipo == "bus" and distancia < 10:
-        print("Stop! there is a bus 10 meters at north, is dangerous!")
-    if tipo == "train" and distancia < 10:
+    elif tipo == "bus" and distancia < 10:
+        print("Stop! there is a bus 10 meters at ahead, is dangerous!")
+    elif tipo == "train" and distancia < 10:
         print("Stop inmedeatly!, there is a train 10 meters in front of you!")
-    if tipo == "truck" and distancia < 10:
+    elif tipo == "truck" and distancia < 10:
         print("Stop!! There is a truck 10 meters away in front you")
+    elif tipo == "traffic light" and distancia < 10:
+        print("Traffic light ahead, stop and wait")
+    else:
+        alerted = False
+# If this log completes the full set of ifs, then the human was not alerted so we put alerted value at False
+
+    if alerted:
+        last_time_alert[tipo] = now
---
+++ ML/voice.py
@@ -0,0 +1,9 @@
+
+# Define a function where we could speak the content of a text
+# Main use, el LLM habla o habla el programa el texto de las alertas hardcodeadas
+
+
+
+
+# Define a function where we could interpret an audio and pass it to text
+# Main use, interpretation for the LLM
